5f0f2ded19f0137170de87492743e9ff30a86527
Manish Kaushik <kausikmanish007@gmail.com>
2025-04-03 12:09:33 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -17,6 +17,14 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes import
+import userRouter from './routes/user.routes.js' 
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+//http://localhot:8000/api/v1/users/register
 
 export { app };
  
